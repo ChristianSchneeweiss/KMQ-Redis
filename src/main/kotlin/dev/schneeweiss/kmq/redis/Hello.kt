b312e6793c6bc0353be3ec4f01116de00e50d19f
Christian Schneeweiß <Schneeweiss97@gmail.com>
2020-05-19 16:24:13 +0200

    project created

---
+++ src/main/kotlin/dev/schneeweiss/kmq/redis/Hello.kt
@@ -0,0 +1,6 @@
+package dev.schneeweiss.kmq.redis
+
+fun main(args: Array<String>) {
+    println("Hello, World")
+}
+
---
+++ src/test/kotlin/dev/schneeweiss/kmq/redis/HelloTest.kt
@@ -0,0 +1,8 @@
+package dev.schneeweiss.kmq.redis
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class HelloTest {
+
+}
